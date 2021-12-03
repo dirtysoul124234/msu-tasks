f55b237235f6d39d4d7d2f21bc5c57b0822ba015
dirtysoul124234 <87073743+dirtysoul124234@users.noreply.github.com>
2021-12-03 20:23:37 +1000

    пупу

---
+++ simple/1main.py
@@ -0,0 +1,52 @@
+
+
+def sort(p: list) -> list:
+    pass
+
+
+def find(p: list, i) -> int:
+    pass
+
+
+def my_min(p: list) -> int:
+    pass
+
+
+def my_max(p: list) -> int:
+    pass
+
+
+def my_sum(p: list) -> int:
+    pass
+
+
+def revert(p: list) -> list:
+    pass
+
+
+def unique(p: list) -> list:
+    pass
+
+
+def capitalize(s: str) -> str:
+    pass
+
+
+def lower(s: str) -> str:
+    pass
+
+
+def upper(s: str) -> str:
+    pass
+
+
+def replace(s: str, old: str, new: str) -> str:
+    pass
+
+
+def swapcase(s: str) -> str:
+    pass
+
+
+def my_split(s, sep):
+    pass
